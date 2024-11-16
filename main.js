4e2723bfdf31d9ad5a16b9ce7406467026d05752
Ian <ianhdez2020@gmail.com>
2024-11-15 22:48:40 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const lista = document.getElementById('lista');
+
+Sortable.create(lista, {
+    animation:150,
+    chosenClass:"seleccionado",
+    dragClass:"drag",
+
+    onEnd: () => {
+        console.log('se inserto');
+    },
+    group:"lista personas",
+    store: {
+        //Guardamos el orden de la lista
+        set: (sortable) => {
+            const orden = sortable.toArray();
+            console.log(orden);
+            localStorage.setItem(sortable.options.group.name, orden.join('-'));
+        },
+        //Obtenemos el orden de la lista
+        get: (sortable) =>{
+            const orden =   localStorage.getItem(sortable.options.group.name);
+            return orden ? orden.split('-') : [];
+        }
+        
+    }
+});
